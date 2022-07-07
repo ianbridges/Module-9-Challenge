faf03252c8f6eccf9eb15888c72cc7d48e9d511d
ianbridges <megabridge12@gmail.com>
2022-07-07 15:44:17 -0500

    index.js start up

---
+++ index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs"
+import generateMarkdown from "./utils/generateMarkdown";
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title your readme'
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
